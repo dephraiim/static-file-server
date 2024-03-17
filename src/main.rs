ff66b168a076b77ecfc6b84f34496b43ab70696c
Ephraim Atta-Duncan <ephraimduncan68@gmail.com>
2024-03-17 12:33:07 +0000

    server with actix web

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+use actix_files::Files;
+use actix_web::{App, HttpServer};
+use std::env;
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    let file_directory = "./static";
+
+    let port = 8000;
+
+    println!("Serving files at http://localhost:{}", port);
+
+    HttpServer::new(move || {
+        App::new().service(Files::new("/", file_directory).show_files_listing())
+    })
+    .bind(("0.0.0.0", port))?
+    .run()
+    .await
 }
